182382787e2db9923e70f31349a1ab1369bd53fe
Aleksandr Beliun <aleksandrbeliun@Aleksandrs-MBP.home>
2019-12-01 20:11:57 +0100

    Installed CKEditor richTextEditor

--- src/js/components/blog/elements/rich-text-editor.jsx
+++ src/js/components/blog/elements/rich-text-editor.jsx
@@ -1,9 +1,29 @@
 import React, { useState } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const RichTextEditor = ({ name }) => {
     return (
         <>
             <p>Rich text editor here: {name}</p>
+            <CKEditor
+                editor={ClassicEditor}
+                data="<p>Hello from CKEditor 5!</p>"
+                onInit={editor => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log('Editor is ready to use!', editor);
+                }}
+                onChange={(event, editor) => {
+                    const data = editor.getData();
+                    console.log({ event, editor, data });
+                }}
+                onBlur={(event, editor) => {
+                    console.log('Blur.', editor);
+                }}
+                onFocus={(event, editor) => {
+                    console.log('Focus.', editor);
+                }}
+            />
         </>
     );
 };
