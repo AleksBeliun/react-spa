54be37d9a65b156fe47797baee3cad3edceda7d9
Aleksandr Beliun <aleksandrbeliun@Aleksandrs-MacBook-Pro.local>
2019-12-04 09:28:20 +0100

    Added logger

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 // import configureStore from './components/blog/store/store.jsx';
 
 import blogListItemsReducer from './components/blog/store/reducers/list-item-reducer.jsx';
 
 import BlogList from "./components/blog/blog-list.jsx";
 
-const store = createStore(blogListItemsReducer);
+
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] Next State ', store.getState());
+            return result;
+        };
+    };
+};
+
+const store = createStore(blogListItemsReducer, applyMiddleware(logger));
 
 class App extends Component {
     constructor() {
